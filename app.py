8558a17ed6cc3fa3dceb78ee7cd701b17f2af5dd
Subrat <subratsinghsks_cse16@its.edu.in>
2022-11-19 22:20:53 -0500

    first rest API

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from data import store
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.get("/test")
 def hello_world():
-    return "<p>Hello, World!</p>"d
+    return {"data":store}
+
+
+@app.post("/store/<string:name>")
+def add_data(name):
+    return name,201
